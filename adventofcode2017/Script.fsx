006ddbf7b56f9bd4c6c6f79dd7ff010c70bd8f00
Kevin Quevedo <kquevedo@hotmail.com>
2017-12-06 22:39:25 +0000

    Created source files

--- adventofcode2017/Script.fsx
+++ adventofcode2017/Script.fsx
@@ -6,3 +6,19 @@ open adventofcode2017
 
 // Define your library scripting code here
 
+let myInt = 5
+let myFloat = 3.14
+let myString = "hello"
+
+let square x = x * x
+
+square 5
+
+let twoToFive = [2;3;4;5]
+let oneToFive = 1::twoToFive
+
+let evens list =
+    let isEven x = x%2 = 0
+    List.filter isEven list
+
+`evens oneToFive
